7c551d59433bad4da04b7ac33cb48e196d9f636a
Nithini Kumarasinghe <54697899+nithinikumarasinghe@users.noreply.github.com>
2026-06-06 21:51:21 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,13 @@ export const metadata: Metadata = {
   alternates: {
     canonical: 'https://enkaybags.com',
   },
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/apple-touch-icon.png' }],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
